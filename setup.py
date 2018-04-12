c8d437fa1ae7f4faee693c7b3e6d943fda79d0c5
Stig Bakken <stig@zedge.net>
2018-04-12 15:39:23 +0200

    Drop pipenv magic in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_packages
-from pipenv.project import Project
-from pipenv.utils import convert_deps_to_pip
 
-pfile = Project(chdir=False).parsed_pipfile
-requirements = convert_deps_to_pip(pfile['packages'], r=False)
-test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
 setup(
     name='kubecd',
     version='0.3',
@@ -14,8 +9,8 @@ setup(
     author_email='stig@zedge.net',
     license='Apache 2',
     packages=find_packages(),
-    install_requires=requirements,
-    tests_require=test_requirements,
+    install_requires=['pyyaml', 'thrift', 'ruamel.yaml', 'semver', 'requests', 'click'],
+    tests_require=['pytest', 'pytest-cov'],
     entry_points={
         'console_scripts': [
             'kcd=kubecd.cli:main'
