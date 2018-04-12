847ac50af23eac7f42b34630356b9af619043e2c
Stig Bakken <stig@zedge.net>
2018-04-12 16:32:30 +0200

    Version 0.5 (drop pyyaml dependency)

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='kubecd',
-    version='0.4',
+    version='0.5',
     description='Kubernetes Continuous Deployment and Inventory Tool',
     url='http://github.com/zedge/kubecd',
     author='Stig Bakken',
     author_email='stig@zedge.net',
     license='Apache 2',
     packages=find_packages(),
-    install_requires=['pyyaml', 'thrift', 'ruamel.yaml', 'semver', 'requests', 'click'],
+    install_requires=['thrift', 'ruamel.yaml', 'semver', 'requests', 'click'],
     tests_require=['pytest', 'pytest-cov'],
     entry_points={
         'console_scripts': [
