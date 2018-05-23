4b669344988ceb80bc6cc5498a93f83a7fc0d1b3
Stig Bakken <stig@zedge.net>
2018-05-23 11:45:39 +0200

    Use subprocess.run() instead of .call()

--- kubecd/github.py
+++ kubecd/github.py
@@ -5,11 +5,11 @@ from typing import List
 
 def pr_from_files(files: List[str], message: str):
     branch_name = 'br%d' % time.time()
-    subprocess.call(['git', 'checkout', '-b', branch_name])
+    subprocess.run(['git', 'checkout', '-b', branch_name], check=True)
     git_add = ['git', 'add']
     git_add.extend(files)
-    print('git_add_cmd:', git_add)
-    subprocess.call(git_add)
-    subprocess.call(['git', 'commit', '-m', message])
-    subprocess.call(['git', 'push', 'origin', branch_name])
-    subprocess.call(['hub', 'pull-request', '-m', message])
+    # print('git_add_cmd:', git_add)
+    subprocess.run(git_add, check=True)
+    subprocess.run(['git', 'commit', '-m', message], check=True)
+    subprocess.run(['git', 'push', 'origin', branch_name], check=True)
+    subprocess.run(['hub', 'pull-request', '-m', message], check=True)
