8cae4f07f1fff536a0b736ec08676a020df33a57
Stig Bakken <stig@zedge.net>
2019-07-02 13:58:26 +0200

    fix copyright

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 Stig Bakken <stig@zedge.net>
+Copyright © 2019 Zedge, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
