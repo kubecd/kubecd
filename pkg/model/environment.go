600f0f8924c2cdfe9f6903681c8d5060ddd9f265
Stig Bakken <stig@stigbakken.com>
2019-11-09 10:05:10 +0100

    Bugfix: duplicate release names were not properly detected (#24)

--- pkg/model/environment.go
+++ pkg/model/environment.go
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2019 Zedge, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package model
 
 import (
@@ -54,11 +69,12 @@ func (e *Environment) populateReleases() error {
 
 func (e *Environment) sanityCheck() []error {
 	var issues []error
-	seenEnv := make(map[string]bool)
+	seenRelease := make(map[string]bool)
 	for _, rel := range e.Releases {
-		if _, seen := seenEnv[rel.Name]; seen {
-			issues = append(issues, fmt.Errorf(`environment %q: duplicate release name %q`, e.Name, rel.Name))
+		if _, seen := seenRelease[rel.Name]; seen {
+			issues = append(issues, fmt.Errorf(`duplicate release %q in environment %q`, rel.Name, e.Name))
 		}
+		seenRelease[rel.Name] = true
 	}
 	return issues
 }
